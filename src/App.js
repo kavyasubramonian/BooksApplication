79d948c577f6c8ac49fa46f1cc60eaabce8b394c
kavyasubramonian <kavyashree.subramonian@nineleaps.com>
2021-05-02 21:23:11 +0530

    Implemented Nav Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Books from './containers/Books';
+import Nav from './components/Nav';
 import CreateBook from './containers/CreateBook';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -8,12 +9,26 @@ import './App.css';
 class App extends Component {
   constructor(props){
     super(props);
+    this.state={
+      pathname: ''
+    }
+    this.notifyPathname = this.notifyPathname.bind(this);
+  }
+
+  notifyPathname(pathname){
+    this.setState({
+      pathname: pathname
+    });
   }
 
   render(){
     return (
       <Router>
         <div className="App">
+          <Nav
+            notifyPathname={this.notifyPathname}
+            pathname={this.state.pathname}
+          />
           <Switch>
             <Route path="/"
                    exact
