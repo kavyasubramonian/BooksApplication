deb986aa39f22aa24ce89ea5f7cb928a9e8e334b
kavyasubramonian <kavyashree.subramonian@nineleaps.com>
2021-05-02 20:51:55 +0530

    Added CreateBook, Book and Books Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Books from './containers/Books';
+import CreateBook from './containers/CreateBook';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Test
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Books/>
+        <CreateBook/>
+      </div>
+    );
+  }
 }
 
 export default App;
